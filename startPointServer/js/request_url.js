311bee609a4ccb2dce9d170629ba0111b1f3f4b6
zhaobod1 <zhaobod1@163.com>
2017-01-02 21:42:29 +0800

    xianStartPointServer v1.0.3

--- startPointServer/js/request_url.js
+++ startPointServer/js/request_url.js
@@ -1,7 +1,7 @@
 
 /* 请求服务器数据url*/
-var request_url = "http://www.qidiancar.com/web/getdata.aspx?com=";
+var request_url = "http://xian.huo15.com/web/getdata.aspx?com=";
 //var request_url = "http://192.168.1.107:88/web/getdata.aspx?com=";
 /* 请求服务器图片url*/
-var request_img_url = "http://www.qidiancar.com/";
+var request_img_url = "http://xian.huo15.com/";
 //var request_img_url = "http://192.168.1.107:88/";
