142e6e8a5a97d0242bac312ba75166c06e4790d2
zhaobo <zhaobod1@163.com>
2017-03-16 02:38:56 +0800

    before add choose carType.

--- startPoint/js/navigate.js
+++ startPoint/js/navigate.js
@@ -2,6 +2,13 @@
     navigate.js 处理导航页面跳转
    *********************************************************************/
 window.onload = function() {
+	//初始化预加载详情页面
+	mui.init({
+		preloadPages: [{
+			id: 'index.html',
+			url: 'index.html'
+		}]
+	});
 	// 顺风车点击事件
 	freeride_fun();
 	// 附近点击事件 
@@ -26,13 +33,23 @@ window.onload = function() {
 	//	navigate_share_fun()
 }
 
-// 顺风车点击事件 
+// 顺风车点击事件 -->修改成出租车
 function freeride_fun() {
 	var tb_freeride = document.getElementById("tb_freeride");
 	tb_freeride.addEventListener("tap", function() {
-		mui.openWindow({
-			url: "navigate/freeride.html"
+		var indexPage = null;
+		if(!indexPage) {
+			indexPage = plus.webview.getWebviewById('index.html');
+		}
+		//触发详情页面的chooseCarType事件
+		mui.fire(indexPage, 'chooseCarType', {
+			carType: 0
 		});
+		//打开详情页面          
+		mui.openWindow({
+			id: 'index.html'
+		});
+		
 	});
 }
 // 代驾点击事件
@@ -40,7 +57,8 @@ function selfdriver_fun() {
 	var tb_selfdriver = document.getElementById("tb_selfdriver");
 	tb_selfdriver.addEventListener("tap", function() {
 		mui.openWindow({
-				url: "index.html"
+			url: "index.html"
+
 		});
 	});
 }
@@ -68,13 +86,11 @@ function longdriver_fun() {
 	var tb_longdriver = document.getElementById("tb_longdriver");
 	tb_longdriver.addEventListener("tap", function() {
 		mui.openWindow({
-//			url: "navigate/longdriver.html"
+			//			url: "navigate/longdriver.html"
 			url: "index.html"
 		});
 	});
 }
-
-
 
 // 门店点击事件
 function subarea_fun() {
@@ -102,11 +118,11 @@ function personcenter_fun() {
 	tab_person.addEventListener("tap", function() {
 		var islogin = plus.storage.getItem('islogin');
 		/*判空操作*/
-		if (islogin == null) {
+		if(islogin == null) {
 			plus.storage.setItem('islogin', 0);
 		}
 		console.log("islogin0====" + islogin);
-		if ((islogin != 1) || (islogin != "1")) {
+		if((islogin != 1) || (islogin != "1")) {
 			mui.openWindow({
 				id: "login",
 				url: "login/login.html"
@@ -127,11 +143,11 @@ function navigate_personcenter_fun() {
 		var islogin = plus.storage.getItem('islogin');
 
 		/*判空操作*/
-		if (islogin == null) {
+		if(islogin == null) {
 			plus.storage.setItem('islogin', 0);
 		}
 		console.log("islogin0====" + islogin);
-		if ((islogin != 1) || (islogin != "1")) {
+		if((islogin != 1) || (islogin != "1")) {
 			mui.openWindow({
 				id: "login",
 				url: "login/login.html"
@@ -154,111 +170,115 @@ function navigate_share_fun() {
 		});
 	});
 }
-var shares=null,bhref=false;
-var Intent=null,File=null,Uri=null,main=null;
+var shares = null,
+	bhref = false;
+var Intent = null,
+	File = null,
+	Uri = null,
+	main = null;
 
 // H5 plus事件处理
-function plusReady(){
+function plusReady() {
 	updateSerivces();
-	if(plus.os.name=="Android"){
+	if(plus.os.name == "Android") {
 		Intent = plus.android.importClass("android.content.Intent");
 		File = plus.android.importClass("java.io.File");
 		Uri = plus.android.importClass("android.net.Uri");
 		main = plus.android.runtimeMainActivity();
 	}
 }
-if(window.plus){
+if(window.plus) {
 	plusReady();
-}else{
-	document.addEventListener("plusready",plusReady,false);
+} else {
+	document.addEventListener("plusready", plusReady, false);
 }
 /**
  * 更新分享服务
  */
-function updateSerivces(){
-	plus.share.getServices( function(s){
-		shares={};
-		for(var i in s){
-			var t=s[i];
-			shares[t.id]=t;
+function updateSerivces() {
+	plus.share.getServices(function(s) {
+		shares = {};
+		for(var i in s) {
+			var t = s[i];
+			shares[t.id] = t;
 		}
-	}, function(e){
-		
-		console.log("获取分享服务列表失败："+e.message );
-	} );
+	}, function(e) {
+
+		console.log("获取分享服务列表失败：" + e.message);
+	});
 }
 // 打开分享
-function shareShow(){
-	bhref=true;
-	var ids=[{id:"weixin",ex:"WXSceneSession"},{id:"weixin",ex:"WXSceneTimeline"},{id:"sinaweibo"},{id:"tencentweibo"}],
-	bts=[{title:"发送给微信好友"},{title:"分享到微信朋友圈"},{title:"分享到新浪微博"},{title:"分享到腾讯微博"}];
-//	if(plus.os.name=="iOS"){
-//		ids.push({id:"qq"});
-//		bts.push({title:"分享到QQ"});
-//	}
-	plus.nativeUI.actionSheet({cancel:"取消",buttons:bts},
-		function(e){
-			var i=e.index;
-			if(i>0){
-				shareAction(ids[i-1].id,ids[i-1].ex);
+function shareShow() {
+	bhref = true;
+	var ids = [{ id: "weixin", ex: "WXSceneSession" }, { id: "weixin", ex: "WXSceneTimeline" }, { id: "sinaweibo" }, { id: "tencentweibo" }],
+		bts = [{ title: "发送给微信好友" }, { title: "分享到微信朋友圈" }, { title: "分享到新浪微博" }, { title: "分享到腾讯微博" }];
+	//	if(plus.os.name=="iOS"){
+	//		ids.push({id:"qq"});
+	//		bts.push({title:"分享到QQ"});
+	//	}
+	plus.nativeUI.actionSheet({ cancel: "取消", buttons: bts },
+		function(e) {
+			var i = e.index;
+			if(i > 0) {
+				shareAction(ids[i - 1].id, ids[i - 1].ex);
 			}
 		}
 	);
 }
 /**
-   * 分享操作
-   * @param {String} id
-   */
-function shareAction(id,ex) {
-	var s=null;
-//	outSet( "分享操作：" );
-	if(!id||!(s=shares[id])){
-		mui.toast( "无效的分享服务！" );
+ * 分享操作
+ * @param {String} id
+ */
+function shareAction(id, ex) {
+	var s = null;
+	//	outSet( "分享操作：" );
+	if(!id || !(s = shares[id])) {
+		mui.toast("无效的分享服务！");
 		return;
 	}
-	if ( s.authenticated ) {
-		console.log( "---已授权---" );
-		shareMessage(s,ex);
+	if(s.authenticated) {
+		console.log("---已授权---");
+		shareMessage(s, ex);
 	} else {
-		console.log( "---未授权---" );
-		s.authorize( function(){
-				shareMessage(s,ex);
-			},function(e){
-				var msgs = e.message.split("http")[0];
-			mui.toast( "认证授权失败："+msgs);
+		console.log("---未授权---");
+		s.authorize(function() {
+			shareMessage(s, ex);
+		}, function(e) {
+			var msgs = e.message.split("http")[0];
+			mui.toast("认证授权失败：" + msgs);
 		});
 	}
 }
-var sharehref="http://xian.huo15.com/down.aspx";
-var sharecontent="我正在使用起点专车客户端,使用租车服务特别方便，赶紧跟我一起来体验！";
-var sharehrefTitle="起点起点出行客户端";
-var sharehrefDes="我正在使用起点出行客户端,使用租车服务特别方便，赶紧跟我一起来体验！";
+var sharehref = "http://xian.huo15.com/down.aspx";
+var sharecontent = "我正在使用起点专车客户端,使用租车服务特别方便，赶紧跟我一起来体验！";
+var sharehrefTitle = "起点起点出行客户端";
+var sharehrefDes = "我正在使用起点出行客户端,使用租车服务特别方便，赶紧跟我一起来体验！";
 /**
-   * 发送分享消息
-   * @param {plus.share.ShareService} s
-   */
-function shareMessage(s,ex){
-	var msg={content:sharecontent,extra:{scene:ex}};
-	if(bhref){
-		msg.href=sharehref;
-		if(sharehrefTitle!=""){
-			msg.title=sharehrefTitle;
+ * 发送分享消息
+ * @param {plus.share.ShareService} s
+ */
+function shareMessage(s, ex) {
+	var msg = { content: sharecontent, extra: { scene: ex } };
+	if(bhref) {
+		msg.href = sharehref;
+		if(sharehrefTitle != "") {
+			msg.title = sharehrefTitle;
 		}
-		if(sharehrefDes!=""){
-			msg.content=sharehrefDes;
+		if(sharehrefDes != "") {
+			msg.content = sharehrefDes;
 		}
-		msg.thumbs=["_www/logo.png"];
-		msg.pictures=["_www/logo.png"];
-	}else{
-		if(pic&&pic.realUrl){
-			msg.pictures=[pic.realUrl];
+		msg.thumbs = ["_www/logo.png"];
+		msg.pictures = ["_www/logo.png"];
+	} else {
+		if(pic && pic.realUrl) {
+			msg.pictures = [pic.realUrl];
 		}
 	}
 	console.log(JSON.stringify(msg));
-	s.send( msg, function(){
-		mui.toast( "分享到\""+s.description+"\"成功！ " );
-	}, function(e){
+	s.send(msg, function() {
+		mui.toast("分享到\"" + s.description + "\"成功！ ");
+	}, function(e) {
 		var msgs = e.message.split("http")[0];
-		mui.toast("分享到\"" + s.description + "\"失败:"+msgs);
-	} );
+		mui.toast("分享到\"" + s.description + "\"失败:" + msgs);
+	});
 }
